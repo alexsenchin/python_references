9016817a095a03a8ac44d4ac7f37d020b0aaa10d
Alexander Senchin <75997616+alexsenchin@users.noreply.github.com>
2022-04-05 20:08:59 +0300

    05.04.22-20:08

--- test_scarper.py
+++ test_scarper.py
@@ -17,23 +17,8 @@ def get_line(n):
     f.close()
 
 
-get_line(0)
-get_line(6)
-get_line(12)
-get_line(18)
-get_line(24)
-get_line(30)
-get_line(36)
-get_line(42)
-get_line(48)
-get_line(54)
-get_line(60)
-get_line(66)
-get_line(72)
-get_line(78)
-get_line(84)
-get_line(90)
-get_line(96)
-get_line(102)
-get_line(108)
-get_line(114)
+n = 0
+
+while n < 115:
+    get_line(n)
+    n = n + 6
